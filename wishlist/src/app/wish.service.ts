ac4c7d7c4b124b1d5e0983ecb330feb119124a21
roommate6 <floginstefan@gmail.com>
2023-11-25 13:03:18 +0200

    this is how you add options to the http methods

--- wishlist/src/app/wish.service.ts
+++ wishlist/src/app/wish.service.ts
@@ -1,13 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Wish } from '../shared/models/Wish';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishService {
+  private readonly DEFAULT_OPTIONS: any = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
   constructor(private http: HttpClient) {}
 
   getWishes() {
-    return this.http.get('assets/wishes.json');
+    let options: any = this.DEFAULT_OPTIONS;
+
+    options.params = new HttpParams({ fromObject: { format: 'json' } });
+
+    return this.http.get('assets/wishes.json', options);
+  }
+
+  addWish(wish: Wish) {
+    let options: any = this.DEFAULT_OPTIONS;
+
+    options.headers = options.headers.set('Authorization', 'value');
+    this.http.post('assets/wishes.json', wish, options);
   }
 }
